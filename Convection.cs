1b7c7decc06bc32db42d780dfe605123a9c7004a
clatterrr <40931228+clatterrr@users.noreply.github.com>
2020-10-14 20:06:29 +0800

    添加速度变量

---
+++ Convection.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Convection : MonoBehaviour
+{
+    public GameObject prefab;
+    private float PlaneSize = 10.0f;
+    private const int QuadNumber = 16; //箭头在一个方向上的数量
+    private GameObject[,] Arrow = new GameObject[QuadNumber, QuadNumber];
+    private float[] ArrowRotation = new float[QuadNumber * QuadNumber];
+    private float[] ArrowScale = new float[QuadNumber * QuadNumber];
+
+    public Material ConvectionMat;//用于绘制平流
+    public Material CheckerInit;//用于绘制初始棋盘格
+    public Material CheckerMove;//用于绘制初始棋盘格
+
+    public RenderTexture RtPreChecker;//棋盘格前一帧
+    public RenderTexture RtNowChecker;//棋盘格这一帧
+
+    public RenderTexture RtPreFrame;//前一帧图像
+    public RenderTexture RtNowFrame;//本帧图像
+    void Start()
+    {
+        Application.targetFrameRate = 10;
+        float QuadSize = PlaneSize / QuadNumber;
+
+        for (int j = 0; j < QuadNumber; j++)
+        {
+            for (int i = 0; i < QuadNumber; i++)
+            {
+                float posy = j * QuadSize - PlaneSize / 2.0f + QuadSize / 2.0f;
+                float posx = i * QuadSize - PlaneSize / 2.0f + QuadSize / 2.0f;
+                Arrow[i, j] = Instantiate(prefab, new Vector3(posx, 0.1f, posy), Quaternion.identity);
+                Arrow[i, j].transform.localScale = new Vector3(2.0f,2.0f,2.0f);
+                Arrow[i, j].transform.Rotate(new Vector3(90.0f, 0.0f, 0.0f));
+                Arrow[i, j].transform.Rotate(new Vector3(0.0f, 0.0f, -90.0f));
+                ArrowScale[j * QuadNumber + i] = 1.0f;
+
+                ArrowRotation[j * QuadNumber + i] = 0.0f;
+                int HalfNumber = QuadNumber / 2;
+                if (i <= HalfNumber + 1 && i >= HalfNumber && j <= HalfNumber + 1 && j >= HalfNumber)
+                {
+                    ArrowScale[j * QuadNumber + i] = 2.0f;
+                }
+                if (i <= HalfNumber + 2 && i >= HalfNumber + 1 && j <= HalfNumber + 2 && j >= HalfNumber + 1)//右上角
+                {
+                    ArrowRotation[j * QuadNumber + i] = 315.0f;
+                }
+                if (i <= HalfNumber && i >= HalfNumber - 1 && j <= HalfNumber + 2 && j >= HalfNumber + 1)//左上角
+                {
+                    ArrowRotation[j * QuadNumber + i] = 45.0f;
+                }
+                if (i <= HalfNumber && i >= HalfNumber - 1 && j <= HalfNumber && j >= HalfNumber - 1)//左下角
+                {
+                    ArrowRotation[j * QuadNumber + i] = 135.0f;
+                }
+                if (i <= HalfNumber + 2 && i >= HalfNumber + 1 && j <= HalfNumber && j >= HalfNumber - 1)//右下角
+                {
+                    ArrowRotation[j * QuadNumber + i] = 225.0f;
+                }
+                Arrow[i, j].transform.Rotate(new Vector3(0.0f, 0.0f, ArrowRotation[j * QuadNumber + i]));
+                Arrow[i, j].transform.localScale = new Vector3(ArrowScale[j * QuadNumber + i], ArrowScale[j * QuadNumber + i], ArrowScale[j * QuadNumber + i]);
+            }
+        }
+        Graphics.Blit(null, RtPreFrame, CheckerInit);//绘制初始的黑白棋盘格纹理
+        Graphics.Blit(RtPreFrame, RtPreChecker);
+    }
+    private void Update()
+    {
+        ConvectionMat.SetFloatArray("_ArrowRotation", ArrowRotation);
+        ConvectionMat.SetFloatArray("_ArrowScale", ArrowScale);
+        ConvectionMat.SetTexture("_CheckerTex", RtNowChecker);
+    }
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(RtPreChecker, RtNowChecker, CheckerMove);//移动棋盘格
+        Graphics.Blit(RtNowChecker, RtPreChecker);
+
+        Graphics.Blit(RtPreFrame, RtNowFrame, ConvectionMat);//移动之前的纹理
+        Graphics.Blit(RtNowFrame, RtPreFrame);
+        Graphics.Blit(source, destination);
+    }
+}
---
+++ ConvectionShader.shader
@@ -0,0 +1,90 @@
+﻿Shader "Unlit/ConvectionShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _CheckerTex("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            sampler2D _CheckerTex;
+            float4 _MainTex_ST;
+            uniform float _ArrowRotation[256];
+            uniform float _ArrowScale[256];
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float uvx, uvy;
+
+            int _TexelNumber = 16;
+            int intuvx = floor(i.uv.x * _TexelNumber);
+            int intuvy = floor(i.uv.y * _TexelNumber);
+            float rad = _ArrowRotation[intuvy * _TexelNumber + intuvx] * 3.1415927f / 180.0f;
+            float velocity = _ArrowScale[intuvy * _TexelNumber + intuvx];
+            float cosRes = cos(rad);
+            float sinRes = sin(rad);
+            bool HighRes = false;
+            if (HighRes)
+            {
+                int cosRange07 = floor(cosRes * 8.0f);
+                int sinRange07 = floor(sinRes * 8.0f);
+                cosRes = cosRange07 / 8.0f; //保证是1/8的倍数，从1/8到7/8
+                sinRes = sinRange07 / 8.0f;
+            }
+
+
+            uvx = i.uv.x - 1.0f / 256.0f * cosRes * velocity;
+            uvy = i.uv.y - 1.0f / 256.0f * sinRes * velocity;
+            float4 col;
+                if (uvx < 0)
+                {
+                    uvx += 1.0f;
+                    col  = tex2D(_CheckerTex, float2(uvx, uvy));
+                }
+                else
+                {
+                    col =  tex2D(_MainTex, float2(uvx, uvy));
+                }
+                
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
